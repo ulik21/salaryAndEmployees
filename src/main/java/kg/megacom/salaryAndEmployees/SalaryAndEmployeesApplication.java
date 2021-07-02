0015949a07c3b1e9962e560c546f4721e3af00c3
Ulugbek Chynybaev <ulugbekchynybaev@gmail.com>
2021-07-02 09:41:36 +0600

    first commit

---
+++ src/main/java/kg/megacom/salaryAndEmployees/SalaryAndEmployeesApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.salaryAndEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryAndEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryAndEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/megacom/salaryAndEmployees/SalaryAndEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.salaryAndEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryAndEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
